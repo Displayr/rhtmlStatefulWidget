8b97bb928ff7b4177acdf2500f3aa0ed78deb288
Bruce Tan <bruce.tan@displayr.com>
2025-03-04 17:43:24 +1300

    DPI-2843 Pack statefulwidget in nix

--- package.nix
+++ package.nix
@@ -5,8 +5,5 @@ pkgs.rPackages.buildRPackage {
   version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
   src = ./.;
   description = "A trivial htmlwidget that is able to store and restore its state";
-  propagatedBuildInputs = with pkgs.rPackages; [ 
-    
- ];
 
 }
